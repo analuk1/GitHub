d0c41105b98b4c88825626c2314d1345c3dd13cb
analuk1 <ana.lukic@hs-furtwangen.de>
2022-11-20 19:11:14 +0100

    auf6

---
+++ Aufgabe6/my-script.js
@@ -0,0 +1,2 @@
+const x = 1;
+//# sourceMappingURL=my-script.js.map
---
+++ Aufgabe6/my-script.ts
@@ -0,0 +1,43 @@
+function Germany(a, b, c, d) {
+    var result = ""
+    result += a + " Waldfläche in Deutschland " 
+    result += b + " Relativ zur Gesamtzahl in der EU "
+    result += c + " Baumverlust seit 2001 "
+    result += d + " Baumverlust gesammt zwischen 2001 und 2021 "
+    return result;
+}
+
+console.log(Germany("11.4 mHa","7.1%", "9.1%", "1.14 mHa"))
+
+function France(a, b, c, d) {
+    var result = ""
+    result += a + " Waldfläche in Frankreich " 
+    result += b + " Relativ zur Gesamtzahl in der EU "
+    result += c + " Baumverlust seit 2001 "
+    result += d + " Baumverlust gesammt zwischen 2001 und 2021 "
+    return result;
+}
+
+console.log(France("17.1 mHa","10.7%", "7.9%", "1.33 mHa"))
+
+function Italy(a, b, c, d) {
+    var result = ""
+    result += a + " Waldfläche in Italien " 
+    result += b + " Relativ zur Gesamtzahl in der EU "
+    result += c + " Baumverlust seit 2001 "
+    result += d + " Baumverlust gesammt zwischen 2001 und 2021 "
+    return result;
+}
+
+console.log(Italy("11 mHa","6.9%", "4.5%", "415 kHa"))
+
+function Croatia(a, b, c, d) {
+    var result = ""
+    result += a + " Waldfläche in Italien " 
+    result += b + " Relativ zur Gesamtzahl in der EU "
+    result += c + " Baumverlust seit 2001 "
+    result += d + " Baumverlust gesammt zwischen 2001 und 2021 "
+    return result;
+}
+
+console.log(Croatia("2.7 mHa","1.7%", "3.6%", "87.6 kHa"))
